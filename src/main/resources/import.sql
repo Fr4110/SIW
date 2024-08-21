88bf7fbfb48bd4dea59d14340c9bdac63bb7c33e
Fr4110 <129081717+Fr4110@users.noreply.github.com>
2024-08-21 20:59:02 +0200

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into team(id,name,year,address) values(nextval('team_sequence'), 'Milan',  1899,'street 1');
+insert into team(id,name,year,address) values(nextval('team_sequence'), 'Roma', 1927,'street 2');
+insert into team(id,name,year,address) values(nextval('team_sequence'), 'Inter', 1908,'street 3');
+insert into team(id,name,year,address) values(nextval('team_sequence'), 'Lazio', 1900,'street 2');
